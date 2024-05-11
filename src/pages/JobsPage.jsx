7efe7746ec1b44507a84c7224ebff2c450091079
LalaSabuhi <yavuzadpu@gmail.com>
2024-05-11 17:04:42 +0400

    last commit

--- src/pages/JobsPage.jsx
+++ src/pages/JobsPage.jsx
@@ -1,7 +1,7 @@
 import JobListings from '../components/JobListings'
 import React from 'react'
 
-const JobsPage = ({isHome = false}) => {
+const JobsPage = () => {
 
   return (
     <section className="bg-blue-50 px-4 py-6">
